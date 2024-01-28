01afecc435a4cf165689a34841baa0be24064c88
Atharva Mishra <atharvamishra956+1@gmail.com>
2024-01-27 21:31:54 -0600

    created DatabaseType enum to hold different database types for application
    added database_type and database_endpoint fields to Settings struct
    
    added code to ask user which type of database they want to use remote or local

--- src/settings.rs
+++ src/settings.rs
@@ -5,67 +5,92 @@ use std::io::{self, Write};
 use std::process::exit;
 
 #[derive(Clone, Serialize, Deserialize)]
-struct Settings {
-    root_user: Option<String>,
-    root_password: Option<String>,
-    db_name: Option<String>,
-    api_key: Option<String>,
+pub enum DatabaseType {
+    Remote,
+    Local,
 }
 
-pub fn check_settings() -> (String, String, String, String) {
-    let mut api_key: String = String::from("");
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Settings {
+    pub root_user: Option<String>,
+    pub root_password: Option<String>,
+    pub api_key: Option<String>,
+    pub database_type: Option<DatabaseType>,
+    pub database_endpoint: Option<String>,
+}
+
+pub fn check_settings() -> (Settings, String) {
     let mut settings_exists: bool = false;
     let settings: Settings = match fs::read_to_string("settings.json") {
         Ok(contents) => {
             settings_exists = true;
             let json: Settings = serde_json::from_str(&contents).unwrap();
-            api_key = json.api_key.clone().unwrap_or_default();
             json
         }
         Err(_) => Settings {
             root_user: None,
             root_password: None,
-            db_name: None,
             api_key: None,
+            database_type: None,
+            database_endpoint: None,
         },
     };
 
     let mut updated_settings = settings.clone();
 
-    let required_fields = ["root username", "root password", "database name", "api key"];
+    let required_fields = [
+        "root username",
+        "root password",
+        "database type",
+        "database endpoint",
+        "api key",
+    ];
 
     if required_fields.iter().any(|field| match *field {
         "root username" => updated_settings.root_user.is_none(),
         "root password" => updated_settings.root_password.is_none(),
-        "database name" => updated_settings.db_name.is_none(),
         "api key" => updated_settings.api_key.is_none(),
+        "database type" => updated_settings.database_type.is_none(),
+        "database endpoint" => updated_settings.database_endpoint.is_none(),
         _ => false,
     }) {
         if settings_exists {
             println!("Some fields are missing in settings.json. Let's fill them in.");
         } else {
-            println!("Settings do not exists, please answer the following prompts to start.")
+            println!("Settings do not exist, please answer the following prompts to start.");
         }
         for field in required_fields {
             match field {
-                "root username" if updated_settings.root_user.is_none() => {
+                "root username" => {
                     updated_settings.root_user = prompt_user("Set a root username: ");
                 }
-                "root password" if updated_settings.root_password.is_none() => {
+                "root password" => {
                     let password = prompt_user("Set a root password: ");
                     let salt = generate_salt();
                     updated_settings.root_password =
                         hash_password(password.unwrap_or_default(), salt.clone()).ok();
                 }
-                "database name" if updated_settings.db_name.is_none() => {
-                    updated_settings.db_name = prompt_user("Set a database name: ");
-                }
-                "api key" if updated_settings.api_key.is_none() => {
+                "api key" => {
                     let key = prompt_user("Set an API key: ");
-                    api_key = key.clone().unwrap_or_default();
                     let salt = generate_salt();
                     updated_settings.api_key =
                         hash_password(key.unwrap_or_default(), salt.clone()).ok();
+                }
+                "database type" => {
+                    let db_type = prompt_user("Set database type (remote/local): ");
+                    match db_type {
+                        Some(type_) => {
+                            updated_settings.database_type = match type_.as_str() {
+                                "remote" => Some(DatabaseType::Remote),
+                                "local" => Some(DatabaseType::Local),
+                                _ => None,
+                            };
+                        }
+                        None => updated_settings.database_type = None,
+                    }
+                }
+                "database endpoint" => {
+                    updated_settings.database_endpoint = prompt_user("Set the database endpoint: ");
                 }
                 _ => {}
             }
@@ -84,12 +109,7 @@ pub fn check_settings() -> (String, String, String, String) {
             Ok(result) => {
                 if result {
                     println!("Password verified. Starting Rocket Server...");
-                    return (
-                        updated_settings.root_user.clone().unwrap(),
-                        entered_password.clone().unwrap(),
-                        updated_settings.db_name.clone().unwrap(),
-                        api_key,
-                    );
+                    return (updated_settings, entered_password);
                 } else {
                     println!(
                         "Password verification failed. {} attempts remaining.",
